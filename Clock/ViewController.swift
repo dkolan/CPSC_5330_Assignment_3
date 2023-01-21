e5f581ea64afb8d5b9a67bf520b910c5995d9ffe
Dan Kolan <dan.kolan@gmail.com>
2023-01-20 23:09:57 -0600

    top time label updating correctly

--- Clock/ViewController.swift
+++ Clock/ViewController.swift
@@ -10,13 +10,37 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var timeRemainingLabel: UILabel!
+    @IBOutlet weak var dateTime: UILabel!
+    
+    var speed : Float = 1.0
+    var timeLeft : Int?
+    var estimatedTime : Int?
+    var timer: Timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         datePicker.setValue(UIColor.white, forKeyPath: "textColor")
-//        datePicker.setValue(UIColor.black.withAlphaComponent(0.1), forKey: "backgroundColor")
+        
+        timeRemainingLabel.text = "Time Remaining:"
+        
+        getCurrentTime()
     }
 
-
+    @IBAction func startTimer(_ sender: Any) {
+        print(datePicker.countDownDuration)
+    }
+    
+    private func getCurrentTime() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector:#selector(self.currentTime) , userInfo: nil, repeats: true)
+    }
+    
+    @objc func currentTime() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEE, dd MMM yyyy HH:mm:ss"
+        dateTime.text = formatter.string(from: Date())
+    }
+    
 }
 
