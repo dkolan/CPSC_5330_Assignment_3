c9b8a9582ff19fc5ff88dac5560ae8e15c66ab7d
Dan Kolan <dan.kolan@gmail.com>
2023-01-20 20:52:45 -0600

    styled datepicker for both day and night backgrounds

--- Clock/ViewController.swift
+++ Clock/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        datePicker.setValue(UIColor.white, forKeyPath: "textColor")
+        datePicker.setValue(UIColor.lightGray.withAlphaComponent(0.25), forKey: "backgroundColor")
     }
 
 
